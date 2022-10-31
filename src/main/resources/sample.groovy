b2bdafddb0d551bd687bf1e52cebf09cd416bf55
rishi154 <rishi154@gmail.com>
2022-10-31 17:42:49 +0530

    Update sample.groovy

--- src/main/resources/sample.groovy
+++ src/main/resources/sample.groovy
@@ -1,8 +1,17 @@
-def exampleMethod() {
-    println("exampleMethod")
-}
 
-def otherExampleMethod() {
-    println("otherExampleMethod")
+
+class FirstGroovy {
+	static void main(def args){
+		System.out.printf("Hello ..................   ");
+		//accessSecretVersion(projectId, secretId, versionId)
+	}
+    
+    def exampleMethod() {
+        println("exampleMethod")
+    }
+
+    def otherExampleMethod() {
+        println("otherExampleMethod")
+    }
 }
 return this
